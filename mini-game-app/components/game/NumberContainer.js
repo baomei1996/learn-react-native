e34f5a0e4f4d067589d192e20224826ea0017061
BomiAn <qhal4686@gmail.com>
2022-09-15 23:15:31 +0900

    Feat: randnum 생성 & numberContainer component

---
+++ mini-game-app/components/game/NumberContainer.js
@@ -0,0 +1,27 @@
+import {View, Text, StyleSheet} from "react-native";
+import Colors from "../../constants/colors";
+
+function NumberContainer({ children }) {
+  return <View style={styles.container}>
+    <Text>{children}</Text>
+  </View>
+}
+
+export default NumberContainer;
+
+const styles = StyleSheet.create({
+   container: {
+    borderWidth: 4,
+    borzderColor: Colors.primary600,
+    padding: 24,
+    margin: 24,
+    borderRadius: 8,
+    alignItems: 'center',
+    justifyContent: 'center',
+   },
+   numberText: {
+    fontSize: 36,
+    fontWeight: 'bold',
+    color: '#fff',
+   }
+})
--- mini-game-app/screens/GameScreen.js
+++ mini-game-app/screens/GameScreen.js
@@ -1,11 +1,29 @@
 import { Text, View, StyleSheet } from "react-native";
-import Title from "../components/Title";
+import { useState } from "react";
+import Title from "../components/ui/TitleTitle";
+import NumberContainer from "../components/game/NumberContainer";
+import PriamryButton from "../components/ui/PriamryButton";
 
-function GameScreen() {
+function generateRamdomBetween(min, max, exclude) {
+  const rndNum = Math.floor(Math.random() * (max - min)) + min;
+
+  if(rndNum === exclude) {
+    return generateRamdomBetween(min, max, exclude);
+  } else {
+    return rndNum;
+  }
+}
+
+function GameScreen({ userNumber }) {
+  const initialGuess = generateRamdomBetween(1, 100, userNumber);
+  const [currentGuess, setCurrentGuess] = useState(initialGuess);
   return <View style={styles.screen}>
-   <Title></Title>
+   <Title>Opponent's Guess</Title>
+   <NumberContainer>{currentGuess}</NumberContainer>
     <View>
-      <Text>Higher of Rower</Text>
+      <Text>Higher or lower</Text>
+      <PriamryButton>+</PriamryButton>
+      <PriamryButton>-</PriamryButton>
     </View>
   </View>
 }
