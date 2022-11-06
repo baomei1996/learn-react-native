d703160ee6c7e919a00f05c45b4f50a848032e78
baomei1996 <qhal4686@gmail.com>
2022-11-06 16:37:38 +0900

    Feat: styling grid
    -  os별 스타일링 적용
    - multi style
    - style pressed props

--- react-navigation-project/screens/CategoriesScreen.js
+++ react-navigation-project/screens/CategoriesScreen.js
@@ -3,11 +3,11 @@ import { CATEGORIES } from "../data/dummy-data";
 import CategoryGridTile from "../components/CategoryGridTile";
 
 function renderCategoryItem({item}) {
-    return <CategoryGridTile title={item.title}/>
+    return <CategoryGridTile title={item.title} color={item.color}/>
 }
 
 function CategoriesScreen() {
-    return <FlatList data={CATEGORIES} keyExtractor={(item) => item.id} renderItem={renderCategoryItem}/>
+    return <FlatList data={CATEGORIES} keyExtractor={(item) => item.id} renderItem={renderCategoryItem} numColumns={2}/>
 }
 
 export default CategoriesScreen;
