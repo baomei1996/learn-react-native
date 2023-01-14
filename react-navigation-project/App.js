305566df5da39dc62fd9b5137f99de5731981e8d
baomei1996 <qhal4686@gmail.com>
2023-01-14 17:22:42 +0900

    Feat: 주석 제거

--- react-navigation-project/App.js
+++ react-navigation-project/App.js
@@ -30,12 +30,6 @@ export default function App() {
                     <Stack.Screen
                         name="MealsOverview"
                         component={MealsOverviewScreen}
-                        // options={({ route, navigation }) => {
-                        //     const catId = route.params.categoryId;
-                        //     return {
-                        //         title: catId,
-                        //     };
-                        // }}
                     />
                     <Stack.Screen
                         name="MealDetail"
